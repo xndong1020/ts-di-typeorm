56f1185b4350261df8b7561dfe80c88492556c47
Nicole Dong <xndong1020@gmail.com>
2022-08-13 22:23:40 +1000

    fix bug

--- src/routers/UserRouter.ts
+++ src/routers/UserRouter.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import Container from "typedi";
-
-import { UserService } from "../services/UserService";
+import UserController from "../controllers/UserController";
 
 const usersRouter = express.Router();
-const userController = Container.get(UserService);
+const userController = Container.get(UserController);
 
 usersRouter.get("/", async (req, res) => {
   const result = await userController.getAll();
